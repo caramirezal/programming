dea03d4b6d0dfecc672af6372dfb0b078c325a6c
caramirezal <cramireza@ciencias.unam.mx>
2017-09-14 01:17:47 -0500

    intro

---
+++ python/intro.py
@@ -0,0 +1,20 @@
+
+str1 = "Hola"
+
+## subsetting characters from strings
+char1 = str1[2]
+
+## lists 
+ls1 = ["hola","mundo",1,2]
+
+## NOTE: python indexes starts at zero
+
+## tuples
+tup1 = ("Hola","Mundo",1,2)
+
+## dictionaries
+dict1 = {"saludo":"hola","integer":31,"real":0.3}
+
+## for loop
+for i in range(0,10):
+        print(i)
